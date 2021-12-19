0a3bbcfc737f428568d6b53263437c3557d6be71
Kishore.io <nkishore653@gmail.com>
2021-12-19 20:32:17 +0530

    Inches_TO_mm converter

---
+++ Inches_To_mm_Converter/Inches_To_mm_Converter/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace Inches_To_mm_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_input = new System.Windows.Forms.TextBox();
+            this.txt_inches = new System.Windows.Forms.TextBox();
+            this.txt_mm = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_input
+            // 
+            this.txt_input.Location = new System.Drawing.Point(42, 68);
+            this.txt_input.Multiline = true;
+            this.txt_input.Name = "txt_input";
+            this.txt_input.Size = new System.Drawing.Size(100, 65);
+            this.txt_input.TabIndex = 0;
+            this.txt_input.Leave += new System.EventHandler(this.textBox3_Leave);
+            // 
+            // txt_inches
+            // 
+            this.txt_inches.Location = new System.Drawing.Point(121, 175);
+            this.txt_inches.Multiline = true;
+            this.txt_inches.Name = "txt_inches";
+            this.txt_inches.Size = new System.Drawing.Size(100, 65);
+            this.txt_inches.TabIndex = 0;
+            // 
+            // txt_mm
+            // 
+            this.txt_mm.Location = new System.Drawing.Point(196, 286);
+            this.txt_mm.Multiline = true;
+            this.txt_mm.Name = "txt_mm";
+            this.txt_mm.Size = new System.Drawing.Size(100, 65);
+            this.txt_mm.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User(Input)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(244, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(193, 270);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Mm";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(118, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Inches";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 405);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_mm);
+            this.Controls.Add(this.txt_inches);
+            this.Controls.Add(this.txt_input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_input;
+        private System.Windows.Forms.TextBox txt_inches;
+        private System.Windows.Forms.TextBox txt_mm;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Inches_To_mm_Converter/Inches_To_mm_Converter/Form1.cs
@@ -0,0 +1,408 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inches_To_mm_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class UNITS
+        {
+            #region UNITS
+            public static double inch = 0;
+            public static double mm = 0;
+            public static double feet = 0;
+            public static string[] inchValues = null;
+            public static string factorial_Value = String.Empty;
+            public static Dictionary<string, double> all_units = new Dictionary<string, double>();
+            #endregion
+        }
+
+        private void textBox3_Leave(object sender, EventArgs e)
+        {
+            Inch_To_Millimetre(txt_input.Text);
+        }
+        private void Inch_To_Millimetre(string inputs)
+        {
+            #region Inches to mm
+            if (!inputs.Contains('-'))
+            {
+                if (inputs.Contains('\''))
+                {
+                    Initializing_Values();
+
+                    UNITS.inchValues = inputs.Split('\'');
+                    UNITS.feet = Convert.ToDouble(UNITS.inchValues[0]);
+
+                    UNITS.inch = (UNITS.feet * 12);
+                    UNITS.mm = (UNITS.inch * 25.4);
+                    //------------------------------------------------------------------------------------//
+                    txt_inches.Text = Math.Truncate(UNITS.inch).ToString() + "\"";
+                    txt_mm.Text = UNITS.mm.ToString();
+                    return;
+                }
+                else if (inputs.Contains('"'))
+                {
+                    Initializing_Values();
+
+                    if (inputs.Contains("/") && inputs.Contains('\"'))
+                    {
+                        Initializing_Values();
+
+
+                        UNITS.inchValues = inputs.Split('/', '\"');
+                        UNITS.inch = Convert.ToDouble(Convert.ToDouble(UNITS.inchValues[0]) / Convert.ToDouble((UNITS.inchValues[1])));
+                        UNITS.mm = UNITS.inch * 25.4;
+
+                        txt_inches.Text = UNITS.inch.ToString() + "\"";
+                        txt_mm.Text = UNITS.mm.ToString();
+
+                    }
+                    else
+                    {
+                        Initializing_Values();
+                        UNITS.inchValues = inputs.Split('"');
+                        UNITS.inch = Convert.ToDouble(UNITS.inchValues[0]);
+                        UNITS.feet = UNITS.inch / 12;
+                        UNITS.mm = UNITS.inch * 25.4;
+
+                        UNITS.factorial_Value = decimalToFraction(UNITS.inch - Math.Truncate(UNITS.inch));
+
+                        if ((Math.Truncate(UNITS.inch) - (UNITS.inch)) != 0)
+                        {
+                            txt_input.Text = Math.Truncate(UNITS.feet) + "'" + "-" + (UNITS.inch % 12).ToString() + " " + "\"" + UNITS.factorial_Value;
+                            txt_inches.Text = Math.Truncate(UNITS.inch).ToString() + " " + UNITS.factorial_Value + "\"";
+
+                        }
+                        else
+                        {
+                            txt_input.Text = Math.Truncate(UNITS.feet) + "'" + "-" + (UNITS.inch % 12).ToString() + "\"";
+                            txt_inches.Text = Math.Truncate(UNITS.inch).ToString() + "\"";
+                        }
+                        //-----------------------------------------------------------------------------------//
+                        txt_mm.Text = UNITS.mm.ToString();
+
+                    }
+                }
+                else
+                {
+                    Initializing_Values();
+                    if (inputs.Contains("/"))
+                    {
+                        Initializing_Values();
+
+                        UNITS.inchValues = inputs.Split('/', '\"', ' ');
+
+                        if (inputs.Contains(' '))
+                        {
+                            UNITS.inch = Convert.ToDouble(UNITS.inchValues[0]) + Convert.ToDouble(Convert.ToDouble(UNITS.inchValues[1]) / Convert.ToDouble((UNITS.inchValues[2])));
+                            UNITS.mm = UNITS.inch * 25.4;
+                            UNITS.factorial_Value = decimalToFraction(UNITS.inch - Math.Truncate(UNITS.inch));
+
+                            if ((Math.Truncate(UNITS.inch) - (UNITS.inch)) != 0)
+                            {
+                                txt_inches.Text = Math.Truncate(UNITS.inch) + " " + UNITS.factorial_Value.ToString() + "\"";
+                            }
+                            else
+                            {
+                                txt_inches.Text = UNITS.inch.ToString() + "\"";
+                            }
+                            txt_mm.Text = UNITS.mm.ToString();
+                            txt_input.Text = inputs.ToString() + "\"";
+
+
+
+                        }
+                        else
+                        {
+                            UNITS.inch = Convert.ToDouble(Convert.ToDouble(UNITS.inchValues[0]) / Convert.ToDouble((UNITS.inchValues[1])));
+                            UNITS.mm = UNITS.inch * 25.4;
+
+                            UNITS.factorial_Value = decimalToFraction(UNITS.inch - Math.Truncate(UNITS.inch));
+
+                            if ((Math.Truncate(UNITS.inch) - (UNITS.inch)) != 0)
+                            {
+                                txt_inches.Text = UNITS.factorial_Value.ToString() + "\"";
+                            }
+                            else
+                            {
+                                txt_inches.Text = UNITS.inch.ToString() + "\"";
+                            }
+                            txt_mm.Text = UNITS.mm.ToString();
+                            txt_input.Text = inputs.ToString() + "\"";
+
+                        }
+                    }
+                    else
+                    {
+                        Initializing_Values();
+                        UNITS.inch = Convert.ToDouble(inputs);
+                        UNITS.feet = Math.Truncate(UNITS.inch / 12);
+                        UNITS.mm = UNITS.inch * 25.4;
+                        //factorial_Value = decimalToFraction(UNITS.inch - UNITS.inch);
+                        UNITS.factorial_Value = decimalToFraction(UNITS.inch - Math.Truncate(UNITS.inch));
+                        //-----------------------------------//      
+
+                        if ((Math.Truncate(UNITS.inch) - (UNITS.inch)) != 0)
+                        {
+                            txt_input.Text = UNITS.feet.ToString() + "'" + '-' + Math.Truncate(UNITS.inch % 12).ToString() + " " + UNITS.factorial_Value + '"';
+
+                            txt_inches.Text = Math.Truncate(UNITS.inch).ToString() + " " + (UNITS.factorial_Value).ToString() + '"';
+
+                        }
+                        else
+                        {
+                            txt_input.Text = UNITS.feet.ToString() + "'" + '-' + Math.Truncate(UNITS.inch % 12).ToString() + '"';
+
+                            txt_inches.Text = (UNITS.inch).ToString() + '"';
+
+                        }
+
+                        txt_mm.Text = UNITS.mm.ToString();
+
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                if (inputs.Contains('/') && !inputs.Contains('\'') && !inputs.Contains('"') && !inputs.Contains('-') && !inputs.Contains(' '))
+                {
+                    Initializing_Values();
+
+                    UNITS.all_units.Clear();
+
+                    UNITS.inchValues = inputs.Split('/', '"');
+
+                    double numer = Convert.ToDouble(UNITS.inchValues[0]);
+                    double denom = Convert.ToDouble(UNITS.inchValues[1]);
+
+                    double val = numer / denom;
+
+                    txt_inches.Text = val.ToString();
+                    txt_mm.Text = (val * 25.4).ToString();
+                }
+
+                {
+                    Splited_V(inputs);
+                }
+            }
+            #endregion
+        }
+        private void Splited_V(string input)
+        {
+            if (input.Contains('\'') && input.Contains('"') && input.Contains('-') && input.Contains('/') && input.Contains("\""))
+            {
+                UNITS.all_units.Clear();
+                Initializing_Values();
+
+                UNITS.inchValues = input.Split('/', '\'', '"', '-', ' ');
+
+                UNITS.feet = Convert.ToDouble(UNITS.inchValues[0]);
+                UNITS.inch = Convert.ToDouble(UNITS.feet * 12);
+
+                //if(input.Contains())
+
+                if (input.Contains('\'') && input.Contains('"') && !input.Contains('-'))
+                {
+                    double inch = (Convert.ToDouble(UNITS.inchValues[1]) * 12) + (Convert.ToDouble(UNITS.inchValues[2]) / Convert.ToDouble(UNITS.inchValues[3]));
+
+                    UNITS.mm = inch * 25.4;
+                    UNITS.factorial_Value = decimalToFraction(inch - Math.Truncate(inch));
+
+
+                    if ((Math.Truncate(inch) - (inch)) != 0)
+                    {
+                        txt_inches.Text = Math.Truncate(inch).ToString() + " " + UNITS.factorial_Value;
+                    }
+                    else
+                    {
+                        txt_inches.Text = Math.Truncate(inch).ToString();
+
+                    }
+                    txt_mm.Text = UNITS.mm.ToString();
+
+                }
+                else
+                {
+                    if(input.Contains(' '))
+                    {
+                        double inch = (UNITS.inch + Convert.ToDouble(UNITS.inchValues[2]) + (Convert.ToDouble(UNITS.inchValues[3]) / Convert.ToDouble(UNITS.inchValues[4])));
+
+                        UNITS.mm = inch * 25.4;
+                        UNITS.factorial_Value = decimalToFraction(inch - Math.Truncate(inch));
+
+
+                        if ((Math.Truncate(inch) - (inch)) != 0)
+                        {
+                            txt_inches.Text = Math.Truncate(inch).ToString() + " " + UNITS.factorial_Value;
+                        }
+                        else
+                        {
+                            txt_inches.Text = Math.Truncate(inch).ToString();
+
+                        }
+                        txt_mm.Text = UNITS.mm.ToString();
+                    }
+                    else
+                    {
+                        double inch = (UNITS.inch + Convert.ToDouble(UNITS.inchValues[0]) + (Convert.ToDouble(UNITS.inchValues[2]) / Convert.ToDouble(UNITS.inchValues[3])));
+
+                        UNITS.mm = inch * 25.4;
+                        UNITS.factorial_Value = decimalToFraction(inch - Math.Truncate(inch));
+
+
+                        if ((Math.Truncate(inch) - (inch)) != 0)
+                        {
+                            txt_inches.Text = Math.Truncate(inch).ToString() + " " + UNITS.factorial_Value;
+                        }
+                        else
+                        {
+                            txt_inches.Text = Math.Truncate(inch).ToString();
+
+                        }
+                        txt_mm.Text = UNITS.mm.ToString();
+                    }
+
+                   
+                }             
+
+            }
+            else
+            {
+
+                UNITS.all_units.Clear();
+                Initializing_Values();
+
+                if (input.Contains('/') && !input.Contains('-'))
+                {
+                    //only inches calculation
+
+                    UNITS.inchValues = input.Split('/', '\'', '"', '-', ' ');
+
+                    //UNITS.feet = Convert.ToDouble(UNITS.inchValues[0]);
+                    UNITS.inch = Convert.ToDouble(Convert.ToDouble(UNITS.inchValues[0]));
+
+                    if (input.Contains('\''))
+                    {
+
+                    }
+
+                    double inch = (UNITS.inch) + (Convert.ToDouble(UNITS.inchValues[1]) / Convert.ToDouble(UNITS.inchValues[2]));
+
+                    UNITS.mm = inch * 25.4;
+
+                    UNITS.factorial_Value = decimalToFraction(inch - Math.Truncate(inch));
+
+                    if ((Math.Truncate(inch) - (inch)) != 0)
+                    {
+                        txt_inches.Text = Math.Truncate(inch).ToString() + " " + UNITS.factorial_Value + "\"";
+                    }
+                    else
+                    {
+                        txt_inches.Text = Math.Truncate(inch).ToString() + "\"";
+                    }
+                    txt_input.Text = input + '"';
+                    txt_mm.Text = UNITS.mm.ToString();
+                }
+                else
+                {
+                    UNITS.inchValues = input.Split('/', '\'', '"', '-', ' ');
+
+                    //UNITS.feet = Convert.ToDouble(UNITS.inchValues[0]);
+                    UNITS.inch = Convert.ToDouble(Convert.ToDouble(UNITS.inchValues[0]) * 12);
+                    if (!input.Contains('\''))
+                    {
+                        double inch = (UNITS.inch) + (Convert.ToDouble(UNITS.inchValues[1]));
+                        UNITS.mm = inch * 25.4;
+
+                        UNITS.factorial_Value = decimalToFraction(inch - Math.Truncate(inch));
+
+                        if ((Math.Truncate(inch) - (inch)) != 0)
+                        {
+                            txt_inches.Text = Math.Truncate(inch).ToString() + " " + UNITS.factorial_Value + "\"";
+                        }
+                        else
+                        {
+                            txt_inches.Text = Math.Truncate(inch).ToString() + "\"";
+                        }
+                        //txt_input.Text = UNITS.inchValues[0].ToString() + '\'' + "-" + UNITS.inchValues[1] + "/" + UNITS.inchValues[2] + "\"";
+                        txt_mm.Text = UNITS.mm.ToString();
+                    }
+                    else
+                    {
+                        double inch = (UNITS.inch) + (Convert.ToDouble(UNITS.inchValues[2]));
+                        UNITS.mm = inch * 25.4;
+
+                        UNITS.factorial_Value = decimalToFraction(inch - Math.Truncate(inch));
+
+                        if ((Math.Truncate(inch) - (inch)) != 0)
+                        {
+                            txt_inches.Text = Math.Truncate(inch).ToString() + " " + UNITS.factorial_Value + "\"";
+                        }
+                        else
+                        {
+                            txt_inches.Text = Math.Truncate(inch).ToString() + "\"";
+                        }
+                        //txt_input.Text = UNITS.inchValues[0].ToString() + '\'' + "-" + UNITS.inchValues[1] + "/" + UNITS.inchValues[2] + "\"";
+                        txt_mm.Text = UNITS.mm.ToString();
+                    }
+                }
+                //MessageBox.Show(inch.ToString());
+
+            }
+        }
+
+        private void Initializing_Values()
+        {
+            #region Initializing
+            UNITS.inch = 0;
+            UNITS.mm = 0;
+            UNITS.feet = 0;
+            UNITS.inchValues = null;
+            UNITS.factorial_Value = String.Empty;
+
+            #endregion
+        }
+        public double gcd(double a, double b)A
+        // Function to convert decimal to fraction
+        public string decimalToFraction(double number)
+        {
+            // Fetch integral value of the decimal
+            double intVal = Math.Floor(number);
+
+            // Fetch fractional part of the decimal
+            double fVal = number - intVal;
+
+            // Consider precision value to
+            // convert fractional part to
+            // integral equivalent
+            const long pVal = 1000000000;
+
+            // Calculate GCD of integral
+            // equivalent of fractional
+            // part and precision value
+            double gcdVal
+                = gcd(Math.Round(fVal * pVal), pVal);
+
+            // Calculate num and deno
+            double num
+                = Math.Round(fVal * pVal) / gcdVal;
+            double deno = pVal / gcdVal;
+
+            // Print the fraction
+            return (intVal * deno) + num
+                   + "/" + deno;
+        }
+    }
+}
